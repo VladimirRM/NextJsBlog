4d5344305fa273047b8cd10ce12f3cd267765ec6
Vladimir RM <shvdrummer@gmail.com>
2022-10-19 19:35:39 +0300

    adding next/link

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,5 +1,10 @@
 import styles from './layout.module.css'
 import UtilStyles from './styles/utils.module.css' 
+import Image from 'next/image'
+import Link from "next/link"
+
+
+const name = 'Max Roslow'
 
 
 
@@ -8,9 +13,22 @@ export default function Layout({children, home}){
         <div className={UtilStyles.container}>
             <header className={styles.header}>
                 { home ? (
+                     <>
+                     <Image priority
+                     src='/images/profile.jpeg'
+                     className={UtilStyles.borderCircle}
+                     height={144}
+                     width={144}
+                     alt={name}
+                     />
+                     <h1 className={UtilStyles.heading2xl}>{name}</h1>
+                     </>
 
                 ):(
+                   <>
+                   </>
                     
+
                 )
 
                 }
